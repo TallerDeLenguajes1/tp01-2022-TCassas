4893b8f1caacce88c523353869bad9b9dbe5b3c0
Cassas Tomás <tomascassas@gmail.com>
2022-03-30 20:46:41 -0300

    Punto 3 tp1_1.cpp correciones

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,12 +1,4 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <iostream>
+#include <stdio.h>
 
 int main() {
     printf("Hola mundo\n");
@@ -15,19 +7,19 @@ int main() {
     int * punteroVariableInt = &variableInt;
     
     printf("Contenido del puntero: %d\n", *punteroVariableInt);
-    printf("Direccion de memoria del puntero: %p\n", punteroVariableInt);
-    printf("Direccion de memoria de la variable: %p\n", &variableInt);
+    printf("Direccion de memoria almacenada en el puntero: %p\n", punteroVariableInt);
+    printf("Direccion de memoria de la variableInt: %p\n", &variableInt);
     printf("Direccion de memoria del puntero: %p\n", &punteroVariableInt);
-    printf("Tamaño de la variableInt: %d Bytes\n\n", sizeof(variableInt));
+    printf("Tamaño de la variable: %d Bytes\n\n", sizeof(variableInt));
     
     char variableChar = 'T';
     char * punteroVariableChar = &variableChar;
     
-    printf("Contenido del puntero: %d\n", *punteroVariableChar);
-    printf("Direccion de memoria del puntero: %p\n", punteroVariableChar);
-    printf("Direccion de memoria de la variable: %p\n", &variableChar);
+    printf("Contenido del puntero: %c\n", *punteroVariableChar);
+    printf("Direccion de memoria almacenada en el puntero: %p\n", punteroVariableChar);
+    printf("Direccion de memoria de la variableChar: %p\n", &variableChar);
     printf("Direccion de memoria del puntero: %p\n", &punteroVariableChar);
-    printf("Tamaño de la variableChar: %d Bytes\n", sizeof(variableChar));
+    printf("Tamaño de la variable: %d Bytes\n", sizeof(variableChar));
     
     return 0;
 }
